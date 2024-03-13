634d4158a868237f6e7baf6d9475e008bc3095ff
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-14 00:53:06 +0300

    Change ShareOwnershipSerializer

--- share/api/serializers/share_ownership_serializer.py
+++ share/api/serializers/share_ownership_serializer.py
@@ -7,13 +7,12 @@ from .slot_serializer import SlotSerializer
 class ShareOwnershipSerializer(serializers.ModelSerializer):
     share = ShareSerializer(read_only=True)
     slots = SlotSerializer(read_only=True, many=True)
-    total_profit = serializers.SerializerMethodField()
+    general_status = serializers.SerializerMethodField()
 
-    def get_total_profit(self, obj):
+    @staticmethod
+    def get_general_status(obj):
         remaining_lots = 0
         total_profit = 0
-
-        # Her bir işlemi değerlendirme
         if slots := obj.slots.all():
             for transaction in slots:
                 if transaction.progres_type == transaction.ProgresType.BUY:
@@ -21,18 +20,13 @@ class ShareOwnershipSerializer(serializers.ModelSerializer):
                 else:
                     sold_lots = transaction.quantity
                     sale_price = float(transaction.price)
-
-                    # Satılan lotlar için kar hesapla
                     profit = sold_lots * (sale_price - float(obj.share.current_price))
                     total_profit += profit
-
-                    # Kalan lotları güncelle
                     remaining_lots -= sold_lots
-
-            # Karı ve kalan lot sayısını yazdır
-        print("Toplam Kar:", total_profit)
-        print("Kalan Lot Sayısı:", remaining_lots)
-        return total_profit
+        return {
+            'total_profit': total_profit,
+            'remaining_lots': remaining_lots
+        }
 
     class Meta:
         model = ShareOwnershipModel
