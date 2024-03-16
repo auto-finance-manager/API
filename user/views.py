e5dfd85505f87ca429d25a005defd77831ee8b35
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-16 14:03:45 +0300

    Add Custom Form base

--- user/views.py
+++ user/views.py
@@ -70,3 +70,9 @@ class UserPasswordResetView(PasswordResetView):
 
 class UserPasswordResetConfirmView(PasswordResetConfirmView):
     template_name = 'password_reset_confirm.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UserPasswordResetConfirmView, self).get_context_data(**kwargs)
+        context.update(self.kwargs)
+        return context
+
