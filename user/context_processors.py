ac25be10c6dc2150e13ddda498fb6d8582baf7ad
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-16 20:20:43 +0300

    Fix context - context_processors

--- user/context_processors.py
+++ user/context_processors.py
@@ -4,3 +4,4 @@ from .models import UserProfile
 def profile(request):
     if request.user.is_authenticated:
         return {'profile': UserProfile.objects.get(user=request.user)}
+    return {'profile': None}
