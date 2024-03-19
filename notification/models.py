590c4abd9ee4eb0833b489244cae371caeb398ee
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-19 21:46:55 +0300

    add future-notification

--- notification/models.py
+++ notification/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 class NotificationType(models.Model):
+    permission = models.ManyToManyField('notification.NotificationPermissions')
+
+
+class NotificationPermissions(models.Model):
+    permission = models.SlugField(max_length=150)
+    description = models.CharField(max_length=250)
+
+
+class Notification(models.Model):
     ...
-
-
